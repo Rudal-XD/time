e35529a508819483438dcfca695d311e0055568f
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-07-20 07:40:32 +0700

    Update run.py

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ class License:
 
         # License Pertama
         if self.licensef_input == 'Ryuuou':
-        self.license_name = 'pendragon'
-        self.active = '22-06-2022'
+            self.license_name = 'pendragon'
+            self.active = '22-06-2022'
 
         # License kedua
         elif self.licensef_input == 'Vasillias pendragon':
-        self.license_name = 'pendragon'
-        self.active = '30-06-2022'
+            self.license_name = 'pendragon'
+            self.active = '30-06-2022'
 
         else:
             print('maaf, license yang anda masukan tidak ada!')
